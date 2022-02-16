375d494a3be0f3d971a21931402c74b585e877fe
Kent Pedersen <webzone@kpdesign.dk>
2022-02-16 11:02:23 +0100

    python MIB getting hostname from switch

---
+++ Mib_test_get_hostname/main.py
@@ -0,0 +1,73 @@
+from pysnmp import hlapi
+
+#Connection info for S1
+target1 = '192.168.0.2'
+oids1 = ['.1.3.6.1.2.1.1.5.0']
+credentials1 = hlapi.CommunityData('GRUPPE1')
+
+
+
+#Recieving the value of an individual MIB
+def get(target, oids, credentials, port=161, engine=hlapi.SnmpEngine(), context=hlapi.ContextData()):
+    handler = hlapi.getCmd(
+        engine,
+        credentials,
+        hlapi.UdpTransportTarget((target, port)),
+        context,
+        *construct_object_types(oids)
+    )
+    return fetch(handler, 1)[0]
+
+
+
+
+def construct_object_types(list_of_oids):
+    object_types = []
+    for oid in list_of_oids:
+        object_types.append(hlapi.ObjectType(hlapi.ObjectIdentity(oid)))
+    return object_types
+
+
+
+def fetch(handler, count):
+    result = []
+    for i in range(count):
+        try:
+            error_indication, error_status, error_index, var_binds = next(handler)
+            if not error_indication and not error_status:
+                items = {}
+                for var_bind in var_binds:
+                    items[str(var_bind[0])] = cast(var_bind[1])
+                result.append(items)
+            else:
+                raise RuntimeError('Got SNMP error: {0}'.format(error_indication))
+        except StopIteration:
+            break
+    return result
+
+
+
+def cast(value):
+    try:
+        return int(value)
+    except (ValueError, TypeError):
+        try:
+            return float(value)
+        except (ValueError, TypeError):
+            try:
+                return str(value)
+            except (ValueError, TypeError):
+                pass
+    return value
+
+
+#print(get('192.168.0.2', ['.1.3.6.1.2.1.1.5.0'], hlapi.CommunityData('GRUPPE1')))
+result =get(target1,oids1,credentials1)
+for key in result:
+    print(key)
+    print(result[key])
+
+#print(target)
+# print(oids)
+# print(credentials)
+# print(hlapi.CommunityData('GRUPPE1'))
